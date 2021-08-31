93a9db7a3cf4c34111b664155ad47957d96f780f
Ssipo <88599326+Ssipo@users.noreply.github.com>
2021-08-31 15:59:37 +0100

    Update index.js
    added a line so get a new card works

--- blackJack game/index.js
+++ blackJack game/index.js
@@ -94,6 +94,7 @@ function renderGame() {
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
+        isAlive = true
     } else if (sum === 21) {
         message = "You've got Blackjack!"
         hasBlackJack = true
